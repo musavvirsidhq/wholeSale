4c8ca0810161705e929d904ed54231f22b62213f
musavvir.sidhq <musavvir.sidhq@piserve.com>
2026-07-03 16:48:04 +0530

    add 2nd commit

--- discovery-service/src/main/java/com/msq/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/msq/discovery_service/DiscoveryServiceApplication.java
@@ -3,10 +3,11 @@ package com.msq.discovery_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
