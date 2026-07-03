a4b854b0a2e212eb606cc03bfea136c2a7c9f74b
musavvirsidhq <116044191+musavvirsidhq@users.noreply.github.com>
2026-07-03 15:48:56 +0530

    inital

---
+++ company-user-service/src/test/java/com/msq/company_user_service/CompanyUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msq.company_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
